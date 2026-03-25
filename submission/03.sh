94fc3eb694259562f46327b7fae7f646cd04870f
joie199 <joieama2@gmail.com>
2026-03-25 17:04:09 +0300

    bech32m error

--- submission/03.sh
+++ submission/03.sh
@@ -1,6 +1,6 @@
 # Create a SegWit address.
 # Add funds to the address.
 # Return only the Address
-gtaddr=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" bech32)
+gtaddr=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" bech32m)
 bitcoin-cli -regtest generatetoaddress 101 "$gtaddr"
 echo "$gtaddr"
