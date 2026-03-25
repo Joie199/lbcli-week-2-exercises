5e7c260f377a3af7b9bc7548b0c568cd24db4fbb
joie199 <joieama2@gmail.com>
2026-03-25 16:29:12 +0300

    creating wallet update

--- submission/03.sh
+++ submission/03.sh
@@ -1,8 +1,8 @@
 # Create a SegWit address.
 # Add funds to the address.
 # Return only the Address
-gadd=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress "" bech32)
+gadd=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" bech32)
 bitcoin-cli -regtest generatetoaddress 101 $gadd
-bitcoin-cli -regtest -rpcwallet=builderswallet getalance 
+bitcoin-cli -regtest -rpcwallet=btrustwallet getalance 
 echo $gadd
  
