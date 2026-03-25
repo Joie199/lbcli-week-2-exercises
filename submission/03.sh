68a4021ad33f61b1be9fac2229ac4e139b58a287
joie199 <joieama2@gmail.com>
2026-03-25 16:47:38 +0300

    address invalid

--- submission/03.sh
+++ submission/03.sh
@@ -2,6 +2,7 @@
 # Add funds to the address.
 # Return only the Address
  gadd=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" "bech32")
+ bitcoin-cli -regtest validateaddress "$gadd"
 bitcoin-cli -regtest generatetoaddress 101 "$gadd"
 bitcoin-cli -regtest -rpcwallet=btrustwallet getbalance
 echo $gadd
