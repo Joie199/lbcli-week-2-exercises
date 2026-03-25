4daed13a17647c624e8c593688fbd5c91687ab90
joie199 <joieama2@gmail.com>
2026-03-26 00:03:57 +0300

    Update 03.sh

--- submission/03.sh
+++ submission/03.sh
@@ -3,4 +3,4 @@
 # Return only the Address
 addr=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" bech32)
 echo "address: $addr"
-bitcoin-cli -regtest -rpcwallet=btrustwallet generatetoaddress 101 "$addr"
+bitcoin-cli -regtest generatetoaddress 101 "$addr"
